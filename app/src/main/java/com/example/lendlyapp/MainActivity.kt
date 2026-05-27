07177ccda785ee912e6632b87f7a28ef2da1da69
unknown <franvlcek@gmail.com>
2026-05-26 21:22:12 -0300

    fonts

--- app/src/main/java/com/example/lendlyapp/MainActivity.kt
+++ app/src/main/java/com/example/lendlyapp/MainActivity.kt
@@ -11,7 +11,9 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
 import com.example.lendlyapp.ui.theme.LendlyAppTheme
+import com.example.lendlyapp.ui.theme.interFonts
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -34,7 +36,9 @@ class MainActivity : ComponentActivity() {
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
         text = "Hello $name!",
-        modifier = modifier
+        modifier = modifier,
+        fontFamily = interFonts,
+        fontSize = 24.sp,
     )
 }
 
--- app/src/main/java/com/example/lendlyapp/ui/theme/Type.kt
+++ app/src/main/java/com/example/lendlyapp/ui/theme/Type.kt
@@ -2,11 +2,19 @@ package com.example.lendlyapp.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.example.lendlyapp.R
 
 // Set of Material typography styles to start with
+
+val interFonts = FontFamily(
+    Font(R.font.inter_24_regular, FontWeight.Normal),
+    Font(R.font.inter_24_semi_bold, FontWeight.SemiBold)
+)
+
 val Typography = Typography(
     bodyLarge = TextStyle(
         fontFamily = FontFamily.Default,
