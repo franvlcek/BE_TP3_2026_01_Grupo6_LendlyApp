6de3dce6d14406557712048e553781a07372c534
Javier Niz <nizjavier01@gmail.com>
2026-05-30 19:26:05 -0300

    Javier: Sincronización de logos corregidos y fix final de espaciado en préstamos

--- app/src/main/java/com/example/lendlyapp/pages/home/HomeScreen.kt
+++ app/src/main/java/com/example/lendlyapp/pages/home/HomeScreen.kt
@@ -22,7 +22,6 @@ import com.example.lendlyapp.components.AccountBalanceCard
 import com.example.lendlyapp.components.LoanItem
 import com.example.lendlyapp.components.ProductCard
 import com.example.lendlyapp.ui.theme.interFontsSemiBold
-import com.example.lendlyapp.ui.theme.interFontsRegular
 
 @Composable
 fun HomeScreen(
@@ -57,7 +56,7 @@ fun HomeScreen(
                     companyName = "Nike Inc.",
                     amount = "₱400.00",
                     dueDate = "Fees of February",
-                    logoResId = R.drawable.logo_nike // Aseguramos que sea Nike el que va arriba
+                    logoResId = R.drawable.logo_nike // Ahora que corregiste los nombres, asignamos el correcto
                 )
             }
             
@@ -70,7 +69,7 @@ fun HomeScreen(
                     companyName = "Apple Inc.",
                     amount = "₱1,500.00",
                     dueDate = "Fees of March",
-                    logoResId = R.drawable.logo_apple // Aseguramos que sea Apple el que va abajo
+                    logoResId = R.drawable.logo_apple // Ahora que corregiste los nombres, asignamos el correcto
                 )
             }
 
